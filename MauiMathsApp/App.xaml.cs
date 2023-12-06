2d354ba406f64392c4644108f010e9f84f2620cf
Alexandr Cocier <licoms1997@gmail.com>
2023-12-06 10:15:02 +0200

    Add project files.

---
+++ MauiMathsApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiMathsApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiMathsApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiMathsApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
