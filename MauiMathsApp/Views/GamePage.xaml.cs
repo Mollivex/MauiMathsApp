dacaabfe3706286c406ef4eaa0c33a6346e8a777
Alexandr Cocier <licoms1997@gmail.com>
2023-12-07 22:11:44 +0200

    Add game logic

--- MauiMathsApp/Views/GamePage.xaml.cs
+++ MauiMathsApp/Views/GamePage.xaml.cs
@@ -3,10 +3,53 @@ namespace MauiMathsApp.Views;
 public partial class GamePage : ContentPage
 {
 	public string GameType { get; set; }
-	public GamePage(string gameType)
+	int firstNumber = 0;
+	int secondNumber = 0;
+
+    public GamePage(string gameType)
 	{
 		InitializeComponent();
 		GameType = gameType;
 		BindingContext = this;
+
+		CreateNewQuestion();
 	}
+
+	private void CreateNewQuestion()
+	{
+		var gameOperand = GameType switch
+		{
+			"Addition" => "+",
+			"Substraction" => "-",
+			"Multiplication" => "*",
+            "Division" => "/",
+			_ => ""
+        };
+
+		var random = new Random();
+
+		// Ternary operator
+		firstNumber = GameType != "Division" ? random.Next(1, 999) : random.Next(1, 999);
+		secondNumber = GameType != "Division" ? random.Next(1, 99) : random.Next(1, 99);
+
+        firstNumber = GameType != "Multiplication" ? random.Next(1, 999) : random.Next(1, 99);
+        secondNumber = GameType != "Multiplication" ? random.Next(1, 99) : random.Next(1, 9);
+
+
+        if (GameType == "Division")
+		{
+			while(firstNumber < secondNumber || firstNumber % secondNumber != 0)
+			{
+				firstNumber = random.Next(1, 999);
+				secondNumber = random.Next(1, 99);
+			}
+		}
+
+		QuestionLabel.Text = $"{firstNumber} {gameOperand} {secondNumber}";
+    }
+
+    private void SubmitAnswer_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
